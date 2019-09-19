07ddf4d0d939d8413a7242b25e7d343ac0a729b4
Denis <ignorid@gmai.com>
2019-09-19 23:07:21 +0300

    Initial Commit

---
+++ RGB Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Slider
+//
+//  Created by Денис Иванов on 19.09.2019.
+//  Copyright © 2019 Ivanov Denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
