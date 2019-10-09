5fd49bbacab3b71f6821349aadaae09ae4b6bb1a
Denis <ignorid@gmai.com>
2019-10-09 03:00:25 +0300

    Intermediate version

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -29,19 +29,29 @@ class ViewController: UIViewController {
         
         colorView.layer.cornerRadius = 10
         
-        createToolbar(for: redTextField)
-        createToolbar(for: blueTextField)
-        createToolbar(for: greenTextField)
+        createToolbar(redTextField, greenTextField, blueTextField)
         
         setColorForView()
-        changeValuesTextFields()
-        changeValueLabel()
+        changeValue(for: redTextField, greenTextField, blueTextField)
+        changeValues(for: redSliderLabel, greenSliderLabel, blueSliderLabel)
     }
     
-    @IBAction func valueChanget() {
+    @IBAction func valueChanget(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            redSliderLabel.text = string(from: redSlaider)
+            redTextField.text = string(from: redSlaider)
+        case 1:
+            greenSliderLabel.text = string(from: greenSlaider)
+            greenTextField.text = string(from: greenSlaider)
+        case 2:
+            blueSliderLabel.text = string(from: blueSlaider)
+            blueTextField.text = string(from: blueSlaider)
+        default:
+            break
+        }
+        
         setColorForView()
-        changeValuesTextFields()
-        changeValueLabel()
     }
     
     private func setColorForView() {
@@ -51,53 +61,50 @@ class ViewController: UIViewController {
                                             alpha: 1)
     }
     
-    private func changeValuesTextFields() {
-        redSliderLabel.text = String(format: "%.2f", redSlaider.value)
-        greenSliderLabel.text = String(format: "%.2f", greenSlaider.value)
-        blueSliderLabel.text = String(format: "%.2f", blueSlaider.value)
+    private func changeValues(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0:
+                label.text = string(from: redSlaider)
+            case 1:
+                label.text = string(from: greenSlaider)
+            case 2:
+                label.text = string(from: blueSlaider)
+            default:
+                  break
+            }
+        }
     }
     
-    private func changeValueLabel() {
-        redTextField.text = String(format: "%.2f", redSlaider.value)
-        greenTextField.text = String(format: "%.2f", greenSlaider.value)
-        blueTextField.text = String(format: "%.2f", blueSlaider.value)
+    private func changeValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField.tag {
+            case 0:
+                redTextField.text = string(from: redSlaider)
+            case 1:
+                greenTextField.text = string(from: greenSlaider)
+            case 2:
+                blueTextField.text = string(from: blueSlaider)
+            default:
+                break
+            }
+        }
     }
     
-    private func createToolbar(for textField: UITextField) {
-        let toolbar = UIToolbar(frame:CGRect(x: 0,
-                                             y: 0,
-                                             width: UIScreen.main.bounds.width,
-                                             height: 50))
-        toolbar.barStyle = .default
-        
-        toolbar.items = [
-               UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
-                               target: nil,
-                               action: nil),
-               UIBarButtonItem(barButtonSystemItem: .done,
-                               target: self,
-                               action: #selector(doneWithNumberPad))
-        ]
-           
-        toolbar.sizeToFit()
-        textField.inputAccessoryView = toolbar
-       }
-       
-       @objc func doneWithNumberPad() {
-           view.endEditing(true)
-       }
+    private func string(from slider: UISlider) -> String {
+        return String(format: "%.2f", slider.value)
+    }
 }
 
 extension ViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         
+        changeValues(for: redSliderLabel, greenSliderLabel, blueSliderLabel)
+        
         guard let text = Float(textField.text!) else {
-            
-            changeValuesTextFields()
-            
-            return
-        }
+            changeValue(for: redTextField, greenTextField, blueTextField)
+            return }
         
         switch textField.tag {
         case 0:
@@ -124,14 +131,42 @@ extension ViewController: UITextFieldDelegate {
         if newLength > 4 {
             let alert = UIAlertController(title: "Warning", message: "Invalid number of characters per line. Please enter no more than four characters.", preferredStyle: .alert)
             
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] _ in
-                self?.changeValuesTextFields()
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                self.changeValue(for: self.redTextField, self.greenTextField, self.blueTextField)
             }))
             present(alert, animated: true, completion: nil)
             return false
         }
         
         return newLength <= 4
+    }
+    
+    private func createToolbar(_ textFields: UITextField...) {
+     let toolbar = UIToolbar(frame:CGRect(x: 0,
+                                          y: 0,
+                                          width: UIScreen.main.bounds.width,
+                                          height: 50))
+     toolbar.barStyle = .default
+     
+     toolbar.items = [
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                            target: nil,
+                            action: nil),
+            UIBarButtonItem(barButtonSystemItem: .done,
+                            target: self,
+                            action: #selector(doneWithNumberPad))
+     ]
+        
+     toolbar.sizeToFit()
+     
+     for textField in textFields {
+         textField.inputAccessoryView = toolbar
+     }
+     
+    }
+    
+    @objc func doneWithNumberPad() {
+        view.endEditing(true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
